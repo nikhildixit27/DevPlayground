64bcf4b4e765b56903db547a14cdc325af88ff0b
Nikhil Dixit <nikhildixit2125@gmail.com>
2024-01-23 12:24:24 +0530

    code playground functionality implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Controlled as CodeMirror } from "react-codemirror2";
 import Pusher from "pusher-js";
 import pushid from "pushid";
 import axios from "axios";
 
-import "codemirror/mode/htmlmixed/htmlmixed.js";
-import "codemirror/mode/css/css.js";
-import "codemirror/mode/javascript/javascript.js";
-
 import "./App.css";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+
+import "codemirror/mode/htmlmixed/htmlmixed";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript";
 
 function App() {
-  const [id, setId] = useState(pushid());
+  const [id, setId] = useState("");
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
-  const [output, setOutput] = useState("");
 
-  const runCode = () => {
-    return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-  
-        <style>
-          ${css}
-        </style>
-      </head>
-      <body>
-        ${html}
-        <script type="text/javascript">
-          ${js}
-        </script>
-      </body>
-      </html>
-    `;
-  };
-
-  const codeMirrorOptions = {
-    theme: "material",
-    lineNumbers: true,
-    scrollbarStyle: null,
-    lineWrapping: true,
-  };
-
-  const syncUpdates = () => {
-    const data = { id, html, css, js };
-    axios.post("http://localhost:5000/update-editor", data).catch(console.error);
-  };
+  const iframeRef = useRef(null);
 
   useEffect(() => {
+    setId(pushid());
+
     const pusher = new Pusher("9b387ef6c7edfb8f29b8", {
-      cluster: "ap2",
+      cluster: "eu",
       forceTLS: true,
     });
 
@@ -69,9 +39,65 @@ function App() {
     });
 
     return () => {
+      channel.unbind("text-update");
       pusher.unsubscribe("editor");
     };
   }, [id]);
+
+  useEffect(() => {
+    runCode();
+  }, [html, css, js]);
+
+  const syncUpdates = () => {
+    const data = { id, html, css, js };
+
+    axios.post("http://localhost:5000/update-editor", data).catch(console.error);
+  };
+
+  const runCode = () => {
+    const document = iframeRef.current.contentDocument;
+    const documentContents = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+
+        <script type="text/javascript">
+          ${js}
+        </script>
+      </body>
+      </html>
+    `;
+
+    document.open();
+    document.write(documentContents);
+    document.close();
+  };
+
+  const codeMirrorOptions = {
+    theme: "material",
+    lineNumbers: true,
+    scrollbarStyle: null,
+    lineWrapping: true,
+  };
+
+  const htmlCodeMirrorRef = useRef(null);
+  const cssCodeMirrorRef = useRef(null);
+  const jsCodeMirrorRef = useRef(null);
+
+  const editorWillUnmount = (editor, codeMirror) => {
+    const wrapper = codeMirror.getWrapperElement();
+    wrapper.parentNode.removeChild(wrapper);
+  };
 
   return (
     <div className="App">
@@ -82,48 +108,51 @@ function App() {
             value={html}
             options={{
               mode: "htmlmixed",
-              ...codeMirrorOptions,
+              ...codeMirrorOptions
             }}
-            onBeforeChange={(editor, data, value) => {
-              setHtml(value);
+            onBeforeChange={(editor, data, html) => {
+              setHtml(html);
               syncUpdates();
             }}
+            editorWillUnmount={(editor) => editorWillUnmount(editor, htmlCodeMirrorRef.current)}
+            ref={htmlCodeMirrorRef}
           />
         </div>
-
         <div className="code-editor css-code">
           <div className="editor-header">CSS</div>
           <CodeMirror
             value={css}
             options={{
               mode: "css",
-              ...codeMirrorOptions,
+              ...codeMirrorOptions
             }}
-            onBeforeChange={(editor, data, value) => {
-              setCss(value);
+            onBeforeChange={(editor, data, css) => {
+              setCss(css);
               syncUpdates();
             }}
+            editorWillUnmount={(editor) => editorWillUnmount(editor, cssCodeMirrorRef.current)}
+            ref={cssCodeMirrorRef}
           />
         </div>
-
         <div className="code-editor js-code">
           <div className="editor-header">JavaScript</div>
           <CodeMirror
             value={js}
             options={{
               mode: "javascript",
-              ...codeMirrorOptions,
+              ...codeMirrorOptions
             }}
-            onBeforeChange={(editor, data, value) => {
-              setJs(value);
+            onBeforeChange={(editor, data, js) => {
+              setJs(js);
               syncUpdates();
             }}
+            editorWillUnmount={(editor) => editorWillUnmount(editor, jsCodeMirrorRef.current)}
+            ref={jsCodeMirrorRef}
           />
         </div>
       </section>
-
       <section className="result">
-        <iframe title="result" className="iframe" srcDoc={output} />
+        <iframe title="result" className="iframe" ref={iframeRef} />
       </section>
     </div>
   );
